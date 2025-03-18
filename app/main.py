ba8d24977dc81ec9bd67e2a9daa059fc7742e2ae
viniciusmecosta <costavinicius315@gmail.com>
2025-03-18 11:45:10 -0300

    fix: Api integration with app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
 app = FastAPI(
     title="Peat Data API",
     description="This API intermediates the interaction between Firebase, the app, and the embedded system, and handles notification services.",
     version="1.0.0",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
 )
 
 app.include_router(router)
