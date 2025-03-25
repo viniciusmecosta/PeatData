3acf9a7d6f8f41f91067d32ba2198bb493cdb6ed
viniciusmecosta <costavinicius315@gmail.com>
2025-03-25 19:16:33 -0300

    feat: correct date to aws

--- app/service/level_service.py
+++ app/service/level_service.py
@@ -30,7 +30,7 @@ class LevelService:
 
     def generate_level_data(self):
         end_date = datetime.now()
-        start_date = end_date - timedelta(days=31)
+        start_date = end_date - timedelta(days=31, hours=6)
         current_date = start_date
 
         while current_date <= end_date:
